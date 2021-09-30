dca69ec1f7fd048cf527070af4fabbb74bdadc66
alisattarzadeh7 <alisattarzadeh71@gmail.com>
2021-09-30 23:55:24 +0330

    first commit

---
+++ pages/about/index.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Link from "next/link"
+// eslint-disable-next-line react/display-name
+export default () => {
+
+    return (
+        <div>
+            this is about page
+            <Link href="/">back to home </Link>
+        </div>
+    )
+
+}
